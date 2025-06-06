680ba17a62636c559c1456b6b8abe84386f95f99
shved90 <a.shved90@gmail.com>
2025-06-06 15:22:46 +0100

    bugfix and initial mockup release of navigation helpers for users, adding reset and goback functionality

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,10 +6,13 @@ import Reset from '../assets/reset.png'
 import { useLoginJourneyContext } from '../utils/loginJourneyContext'
 
 const Header = ({ }): ReactElement => {
+    const state = useLoginJourneyContext()
+    
     const navigate = useNavigate();
-    function reset() {
+    const reset = () => {
         localStorage.clear()
         navigate("/")
+        state.setJourney('')
     }
 
     return (
@@ -18,14 +21,14 @@ const Header = ({ }): ReactElement => {
                 <div className='h-12 w-16'>
                     <Logo title="Logo of login demo site" role="img" />
                 </div>
-                {/* <div className="flex gap-x-2">
+                <div className="flex gap-x-2">
                     <button onClick={() => navigate(-1)} className='h-8 w-8'>
                         <Goback title="go back a page" className='max-h-full max-w-full' />
                     </button>
                     <button onClick={() => reset()} className='h-8 w-8'>
                         <img src={Reset} alt="reset the whole app to default" className='max-h-full max-w-full' />
                     </button>
-                </div> */}
+                </div>
             </div>
             <h4 className='mt-6 text-left text-2xl font-normal text-black-500 dark:text-white'>
                 {useLoginJourneyContext().title}
